910bd1f8c6ee28e3d7a13efd41a4ddc6d5d48606
Li Yi FANG <a94118@126.com>
2017-07-05 15:33:23 +0800

    提交#461 Hamming Distance 解答

---
+++ src/main/java/me/leergo/_461/Hamming.java
@@ -0,0 +1,11 @@
+package me.leergo._461;
+
+/**
+ * Created by Xoder on 2017/7/5.
+ */
+public class Hamming {
+    public static void main(String[] args) {
+
+        System.out.println(new Solution().hammingDistance(1, 4));
+    }
+}
