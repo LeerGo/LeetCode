3272a1ad1dc86b8f1b392ca43511ba761e3be757
Li Yi FANG <a94118@126.com>
2017-07-05 16:43:54 +0800

    更新 476 Number Complement

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 2. 461 Hamming Distance - 需要补习位运算 
 3. 561 Array Partition I - 理解题意，有点不好懂
 4. 595 Big Countries - SQL 
+5. 476 Number Complement - 需要补习位运算
---
+++ src/main/java/me/leergo/_476/NumberComplement.java
@@ -0,0 +1,10 @@
+package me.leergo._476;
+
+/**
+ * Created by Xoder on 2017/7/5.
+ */
+public class NumberComplement {
+    public static void main(String[] args) {
+        System.out.println(new Solution().findComplement(5));
+    }
+}
