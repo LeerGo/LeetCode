896d858e0a47d0a5211c9ef4a58350bc7083b7dd
Li Yi FANG <a94118@126.com>
2017-07-06 09:26:39 +0800

    更新 500 Keyboard Row

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 ### LeetCode 刷题记录
 
-1. 535 Encode and Decode TinyURL - 映射关系
-2. 461 Hamming Distance - 需要补习位运算 
-3. 561 Array Partition I - 理解题意，有点不好懂
-4. 595 Big Countries - SQL 
-5. 476 Number Complement - 需要补习位运算
+1. 535 Encode and Decode TinyURL 
+    - 映射关系
+2. 461 Hamming Distance 
+    - 需要补习位运算 
+3. 561 Array Partition I 
+    - 理解题意，有点不好懂
+4. 595 Big Countries 
+    - SQL 
+5. 476 Number Complement 
+    - 需要补习位运算
+6. 500 Keyboard Row 
+    - 正则解
+    - 多遍历循环解 
---
+++ src/main/java/me/leergo/_500/Solution.java
@@ -0,0 +1,40 @@
+package me.leergo._500;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
+/**
+ * <h1>500. Keyboard Row</h1>
+ *
+ * <b>create time : </b>2017-07-05 16:48:18<br>
+ * <b>create by</b> : LeerGo!
+ *
+ * <p>
+ * Given a List of words, return the words that can be typed using letters of alphabet on only one row's
+ * of American keyboard like the image below.
+ * </p>
+ */
+public class Solution {
+
+    public String[] findWords(String[] words) {
+        List<String> res = new ArrayList<>();
+
+        for (String word : words) {
+            if (isOneRow(word)) {
+                res.add(word);
+            }
+        }
+        String[] data = new String[res.size()];
+        for (int i = 0; i < res.size(); i++) {
+            data[i] = res.get(i);
+        }
+        return data;
+    }
+
+    private boolean isOneRow(String word) {
+        String rule = "[qwertyuiop]*|[asdfghjkl]*|[zxcvbnm]*";
+        Pattern pattern = Pattern.compile(rule);
+        return pattern.matcher(word.toLowerCase()).matches();
+    }
+}
