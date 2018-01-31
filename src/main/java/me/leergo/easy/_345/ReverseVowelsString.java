97d74592c324f5c49f0d6842a0cde2fa36f643f9
Li Yi FANG <a94118@126.com>
2018-01-31 11:52:47 +0800

    update: 345.Reverse Vowels String

---
+++ src/main/java/me/leergo/easy/_345/ReverseVowelsString.java
@@ -0,0 +1,7 @@
+package me.leergo.easy._345;
+
+public class ReverseVowelsString {
+    public static void main(String[] args) {
+        System.out.println(new Solution().reverseVowels("hello"));
+    }
+}
