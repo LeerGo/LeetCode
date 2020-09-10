623a6b8c3fd12531a69d3be229f423dd2b966359
Li Yi FANG <a94118@126.com>
2020-09-10 15:12:02 +0800

    #231

--- src/main/java/me/leergo/leetcode/question/easy/PowerOfTwo_231.java
+++ src/main/java/me/leergo/leetcode/question/easy/PowerOfTwo_231.java
@@ -21,17 +21,35 @@
 // Related Topics 位运算 数学 
 // 👍 239 👎 0
 
-package me.leergo.leetcode.editor.cn;
+package me.leergo.leetcode.question.easy;
 
 public class PowerOfTwo_231 {
     public static void main(String[] args) {
         Solution solution = new PowerOfTwo_231().new Solution();
+        System.out.println(solution.isPowerOfTwo(1073741825));
     }
     
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public boolean isPowerOfTwo(int n) {
-            return false;
+            //            if (n == 0) return false;
+            //            while (n % 2 == 0) n /= 2;
+            //            return n == 1;
+            
+            /*
+            二进制如下：
+            2^2   = 4 = 100
+            2^2-1 = 3 = 011
+            
+            与运算：
+                      100
+                    & 011
+                    -----
+                      000
+            又，2^x > 0
+            故此，得出下面解答
+            */
+            return (n > 0) && (n & (n - 1)) == 0;
         }
     }
     //leetcode submit region end(Prohibit modification and deletion)
