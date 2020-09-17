f05062c372392772e4c40560969b41792f77e7be
Li YiFang <a94118@126.com>
2020-09-17 16:12:55 +0800

    #1476、#1480

---
+++ src/main/java/me/leergo/leetcode/question/easy/RunningSumOf1dArray_1480.java
@@ -0,0 +1,70 @@
+//1480 - 一维数组的动态和	 
+
+//给你一个数组 nums 。数组「动态和」的计算公式为：runningSum[i] = sum(nums[0]…nums[i]) 。 
+//
+// 请返回 nums 的动态和。 
+//
+// 
+//
+// 示例 1： 
+//
+// 输入：nums = [1,2,3,4]
+//输出：[1,3,6,10]
+//解释：动态和计算过程为 [1, 1+2, 1+2+3, 1+2+3+4] 。 
+//
+// 示例 2： 
+//
+// 输入：nums = [1,1,1,1,1]
+//输出：[1,2,3,4,5]
+//解释：动态和计算过程为 [1, 1+1, 1+1+1, 1+1+1+1, 1+1+1+1+1] 。 
+//
+// 示例 3： 
+//
+// 输入：nums = [3,1,2,10,1]
+//输出：[3,4,6,16,17]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length <= 1000 
+// -10^6 <= nums[i] <= 10^6 
+// 
+// Related Topics 数组 
+// 👍 27 👎 0
+
+package me.leergo.leetcode.question.easy;
+
+import java.util.Arrays;
+
+public class RunningSumOf1dArray_1480 {
+    public static void main(String[] args) {
+        Solution solution = new RunningSumOf1dArray_1480().new Solution();
+        System.out.println(Arrays.toString(solution.runningSum(new int[]{3, 1, 2, 10, 1})));
+    }
+    
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int[] runningSum(int[] nums) {
+            int sum = 0;
+            for (int i = 1; i < nums.length; i++) {
+                sum += nums[i];
+                nums[i] = sum;
+            }
+            return nums;
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+    /*
+    class Solution {
+        public int[] runningSum(int[] nums) {
+            int[] res = new int[nums.length];
+            res[0] = nums[0];
+            for (int i = 1; i < nums.length; i++) res[i] = nums[i] + res[i - 1];
+            return res;
+        }
+    }
+    */
+}
